5a1f34daeb13cb7b8468da0cb0abe170a564ae99
Ricardo Masao Shigeoka <contato@ricardomasao.com>
2024-10-30 21:48:15 -0300

    Configurando dotenv e dbconnect

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose"
 
-mongoose.connect("mongodb+srv://alura:123@alura.dkjed.mongodb.net/alura-node");
+const DB_CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+console.log(DB_CONNECTION_STRING)
+mongoose.connect(DB_CONNECTION_STRING);
 
 let db = mongoose.connection;
 
